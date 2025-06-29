454dff5e4b3af08bed0b1f67db988e68aa7a8639
Tong <200839284@qq.com>
2025-06-29 17:32:53 +1200

    Update App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,19 @@
-import Home from './pages/Home.tsx'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import Login from './pages/login.tsx';
+import Register from './pages/register.tsx';
+import './App.css';
 
 function App() {
   return (
-    <Home />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
