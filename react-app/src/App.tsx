9937317f8cac0da55b6ab5d1254d33aae13ea42f
Tong <200839284@qq.com>
2025-06-29 17:38:15 +1200

    added layout

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout.tsx';
 import Home from './pages/Home.tsx';
 import Login from './pages/login.tsx';
 import Register from './pages/register.tsx';
@@ -7,11 +8,13 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
